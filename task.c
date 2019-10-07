c25160873005c934495c1a1464fea82dbfb52d3c
VolkanoLiu <hbjllcc@gmail.com>
2019-10-07 23:58:11 +0800

    refactor(UNFINISHED): use a recursion solution.

--- task.c
+++ task.c
@@ -1,70 +1,28 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
+#define MAX_LENGTH 100
 
-// 该函数实现了字符串与字符规律的输入并实现了校验的功能，返回值为1时表示匹配，
-// 0表示不匹配，-1表示格式有误
-// 不能校验".*abcd"这种有歧义的正则表达式
 bool isMatch(char *s, char *p) {
-  char *p_input = s, *p_regex = p;
-  if (*s == '\0' || *p == '\0') {
-    if (*s == '\0' && *p == '\0') {
-      return true;
-    } else {
-      if (p[0] == '.' && p[1] == '*') {
-        return true;
-      } else {
-        return false;
-      }
+  int s_length, p_length;
+  if ((s_length = strlen(s)) && (p_length = strlen(p))) {
+    if (p[p_length - 1] != '*' &&
+        (s[s_length - 1] == p[p_length - 1] || p[p_length - 1] == '.')) {
+      char sn[MAX_LENGTH], pn[MAX_LENGTH];
+      strcpy(sn, s);
+      strcpy(pn, p);
+      sn[s_length - 1] = '\0';
+      pn[p_length - 1] = '\0';
+      return isMatch(sn, pn);
     }
+    // todo
   }
-  do {
-    // 当正则指针指向的字符不是'*'时，该字符串才是有效的正则表达式。
-    // 首先检验当前正则指针指向的字符是一般字符还是'.'，再检验后一位的字符
-    // 是不是'*'，再通过正则指针与输入的后移来实现校验的功能。
-    if (*p_regex != '*') {
-      if (*p_regex != '.') {
-        if (*(p_regex + 1) != '*') {
-          if (*p_regex == *p_input) {
-            p_input++;
-            p_regex++;
-          } else {
-            return false;
-          }
-        } else {
-          while (*p_input == *p_regex) {
-            p_input++;
-          }
-          p_regex += 2;
-        }
-      } else {
-        if (*(p_regex + 1) != '*') {
-          p_input++;
-          p_regex++;
-        } else {
-          return true;
-        }
-      }
-    } else {
-      return false;
-    }
-  } while (*p_regex != '\0');
-  // 当只有部分字符串匹配时报错
-  if (*p_input != '\0') {
-    return false;
-  }
-  return true;
+  // todo
 }
 
 int main() {
-  char string_input[100], string_regex[100];
+  char string_input[MAX_LENGTH], string_rule[MAX_LENGTH];
   gets(string_input);
-  gets(string_regex);
-  bool status = isMatch(string_input, string_regex);
-  if (status == true) {
-    printf("true\n");
-  } else {
-    printf("false\n");
-  }
+  gets(string_rule);
   return 0;
 }
