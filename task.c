45e09b7cf00e9482b223f6a68255db8c777e348e
VolkanoLiu <hbjllcc@gmail.com>
2019-10-08 14:05:42 +0800

    refactor: use recursion to achieve the need.

--- task.c
+++ task.c
@@ -2,18 +2,40 @@
 #include <stdio.h>
 #include <string.h>
 #define MAX_LENGTH 100
-#define CREATE_NEW_ARRAY               \
-  char sn[MAX_LENGTH], pn[MAX_LENGTH]; \
-  strcpy(sn, s);                       \
-  strcpy(pn, p)
 
 bool isMatch(char *s, char *p) {
-  // todo
+  int s_length = strlen(s), p_length = strlen(p);
+  // if s is empty
+  if (s_length == 0) {
+    if (p_length == 0) {
+      return true;
+    } else if (p_length >= 2 && p[1] == '*') {
+      return isMatch(s, p + 2);
+    } else {
+      return false;
+    }
+  }
+  // if p is empty
+  if (p_length == 0) {
+    return s_length == 0;
+  }
+  // if p_length >= 2 and p[1] == '*'
+  if (p_length >= 2 && p[1] == '*') {
+    return isMatch(s, p + 2) ||
+           ((s[0] == p[0] || p[0] == '.') && isMatch(s + 1, p));
+  }
+  // if p[1] is not '*' and p[0] matches s[0]
+  if (s[0] == p[0] || p[0] == '.') {
+    return isMatch(s + 1, p + 1);
+  } else {
+    return false;
+  }
 }
 
 int main() {
   char string_input[MAX_LENGTH], string_rule[MAX_LENGTH];
   gets(string_input);
   gets(string_rule);
+  printf("%d\n", isMatch(string_input, string_rule));
   return 0;
 }
